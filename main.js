d4a8b13008abb2ab41e14230c7c7a3d4ec339cb3
kianakawai1 <104946857+kianakawai1@users.noreply.github.com>
2023-05-04 18:28:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640, 480);
+    canvas.position(110, 250);
+}
+
+function draw(){
+    circle(30, 30, 20);
+describe('white circle with black outline in mid of canvas');
+}
